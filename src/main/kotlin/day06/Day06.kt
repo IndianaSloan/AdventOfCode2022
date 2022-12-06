5ccc80e4769502ca63d9da5746774e2395b9f7d1
Ciaran Sloan <ciaran@fueled.com>
2022-12-06 10:25:17 +0000

    day6 test

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -7,6 +7,9 @@ fun main() {
     fun part1(input: List<String>): Int = input.first().decrypt(4)
     fun part2(input: List<String>): Int = input.first().decrypt(14)
 
+    val testInput = readInput("day06", isSample = true)
+    check(part1(testInput) == 7) { "Part1 Check failed. Result was: ${part1(testInput)}, expected: 7" }
+
     val input = readInput("day06")
     println("Part1: ${part1(input)}")
     println("Part2: ${part2(input)}")
